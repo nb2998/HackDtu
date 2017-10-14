5f07629c196a1207e4bae18b6c3a095e6d6d50d0
Nishtha Bhatia <nbhatia329@gmail.com>
2017-10-14 12:36:03 +0530

    Camera working

---
+++ app/src/main/java/com/apps/nishtha/hackdtu/CameraActivity.java
@@ -0,0 +1,60 @@
+package com.apps.nishtha.hackdtu;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Surface;
+import android.widget.FrameLayout;
+
+public class CameraActivity extends AppCompatActivity {
+    Camera camera;
+    CameraView cameraView;
+    FrameLayout frameLayout;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        camera=Camera.open();
+        if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED) {
+            requestPermissions(new String[]{Manifest.permission.CAMERA}, 123);
+        }
+        if (ActivityCompat.checkSelfPermission(CameraActivity.this,Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+            camera=Camera.open();
+            int currentOrientation = getWindowManager().getDefaultDisplay().getRotation();
+            switch (currentOrientation) {
+                case Surface.ROTATION_0:
+                    camera.setDisplayOrientation(90);
+                    break;
+                case Surface.ROTATION_90:
+                    camera.setDisplayOrientation(0);
+                    break;
+                case Surface.ROTATION_180:
+                    camera.setDisplayOrientation(180);
+                    break;
+                case Surface.ROTATION_270:
+                    camera.setDisplayOrientation(180);
+                    break;
+                default:
+                    camera.setDisplayOrientation(180);
+            }
+        }
+        frameLayout = (FrameLayout) findViewById(R.id.container);
+        cameraView = new CameraView(this, camera);
+        frameLayout.addView(cameraView);
+    }
+
+    @Override
+    protected void onStop() {
+        camera.release();
+        frameLayout.removeView(cameraView);
+        super.onStop();
+    }
+}
--- app/src/main/java/com/apps/nishtha/hackdtu/MainActivity.java
+++ app/src/main/java/com/apps/nishtha/hackdtu/MainActivity.java
@@ -8,27 +8,36 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    Button btnGetSchedule, btnSetSchedule;
+    Button btnGetSchedule, btnSetSchedule, btnCamera;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnGetSchedule= (Button) findViewById(R.id.btnGetSchedule);
-        btnSetSchedule= (Button) findViewById(R.id.btnSetSchecule);
+        btnGetSchedule = (Button) findViewById(R.id.btnGetSchedule);
+        btnSetSchedule = (Button) findViewById(R.id.btnSetSchecule);
+        btnCamera = (Button) findViewById(R.id.btnCamera);
         btnSetSchedule.setOnClickListener(this);
         btnGetSchedule.setOnClickListener(this);
+        btnCamera.setOnClickListener(this);
     }
+
+
 
     @Override
     public void onClick(View view) {
-        if(view.getId()==R.id.btnGetSchedule){
+        if (view.getId() == R.id.btnGetSchedule) {
 
         }
-        if(view.getId()==R.id.btnSetSchecule){
-            Intent i=new Intent(MainActivity.this,AddMedicineActivity.class);
+        if (view.getId() == R.id.btnSetSchecule) {
+            Intent i = new Intent(MainActivity.this, AddMedicineActivity.class);
 //            i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(i);
+        }
+        if (view.getId() == R.id.btnCamera) {
+           Intent i=new Intent(MainActivity.this,CameraActivity.class);
             startActivity(i);
         }
     }
